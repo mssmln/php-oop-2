2c96f9767b562bb10d917794b9f79724337074ae
Massimiliano <m.croci.ly@gmail.com>
2021-03-15 16:15:34 +0100

    instancing a third user

--- User.php
+++ User.php
@@ -1,5 +1,33 @@
 <?php
 
 class User{
-    
+    protected $id;
+    protected $name;
+    protected $surname;
+    protected $email;
+    private $location;
+    public $avatar;
+    protected $anno_iscrizione;
+    protected $messaggi_inviati;
+    protected $like_ricevuti;
+
+
+    public function __construct($_id){
+        $this->id = $_id;
+    }
+
+    public function setProfile($_name,$_surname,$_email){
+        $this->surname = $_surname;
+        $this->email = $_email;
+
+        if(strlen($_surname) < 3 || is_numeric($_email)){
+            throw new Exception('check your data');
+        } else{
+            $this->name = $_name;
+        }
+    }
+
+    public function getProfile(){
+        return $this->name;
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-/*Crea un diagramma per una tabella di db che rappresenti gli Users di un blog (va bene anche un file di testo .txt)
+<?php /*Crea un diagramma per una tabella di db che rappresenti gli Users di un blog (va bene anche un file di testo .txt)
 Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users (quindi create alcune istanze)
-Il database e la tabella non devono essere realmente creati.Bonus: Creare una classe estesa e un trait a piacere. (create anche in questo caso delle istanze)*/
+Il database e la tabella non devono essere realmente creati.Bonus: Creare una classe estesa e un trait a piacere. (create anche in questo caso delle istanze)*/ ?>
 
 <?php
+require_once 'User.php';
+
+
+// istanza utente 1
+$user1 = new User(1);
+//var_dump($user1);
+//var_dump(get_class_methods($user1));
+try{
+    $user1->setProfile('max','redd','mred@email.it');
+} catch(Exception $error){
+    echo $error->getMessage();
+}
+echo $user1->getProfile();
+//var_dump($user1);
+
+
+
+// istanza utente 2
+$user2 = new User(2);
+//$user2->anno_iscrizione = 2021; //cannot access protected property
+$user2->avatar = 'Obama';
+var_dump($user2);
+try{
+    $user2->setProfile('alida','bianchi','alida@email.it');
+} catch(Exception $error){
+    echo $error->getMessage();
+}
+echo $user2->getProfile();
+
+
+
+// istanza utente 3
