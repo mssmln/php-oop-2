d87f2fbbc1874945b0539b87570a85fd1269185a
Massimiliano <m.croci.ly@gmail.com>
2021-03-15 14:48:53 +0100

    init

---
+++ index.php
@@ -0,0 +1,5 @@
+/*Crea un diagramma per una tabella di db che rappresenti gli Users di un blog (va bene anche un file di testo .txt)
+Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users (quindi create alcune istanze)
+Il database e la tabella non devono essere realmente creati.Bonus: Creare una classe estesa e un trait a piacere. (create anche in questo caso delle istanze)*/
+
+<?php
